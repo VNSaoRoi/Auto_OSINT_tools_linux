5504bc89fea1b5f020c84aedc29ba6a58b5b2eef
VNSaoRoi <52862996+VNSaoRoi@users.noreply.github.com>
2022-10-30 20:04:41 +0700

    Update DNS_OSINT_Auto.sh

--- DNS_OSINT_Auto.sh
+++ DNS_OSINT_Auto.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # How to use:
-# ./thisSCRIPT [domain]
+# ./thisSCRIPT [domain] [where-to-save-result]
 echo '  ________ _______ ___      ___ _____  ___   
  /"       /"      |"  \    /"  (\"   \|"  \  
 (:   \___|:        \   \  //  /|.\\   \    | 
@@ -12,21 +12,11 @@ echo '  ________ _______ ___      ___ _____  ___
 
  '
 # Exit if $1 = null:
-if [[ $1 == "" ]]
-then
-        echo '
-	-h: for help
-        '
-        exit
-fi
-
-
-# Show help menu eand exit:
-if [[ $1 == "-h" ]]
+if test "$1" = "" || test "$2" = ""; 
 then
 	echo '
 	This script will auto find subdomain for you by OSINT.
-	Just run command ./thisSCRIPTname [domain].
+	Just run command ./thisSCRIPTname [domain] [where-to-save-result].
 	'
 	exit
 fi
@@ -34,24 +24,41 @@ fi
 export OSINTDNSTARGET=$1
 
 # Create source: 
-echo 'baidu
+echo 'anubis
+baidu
+bevigil
+binaryedge
+bing
+bingapi
 bufferoverun
+censys
+certspotter
 crtsh
+dnsdumpster
+duckduckgo
+fullhunt
+github-code
 hackertarget
+hunter
+intelx
+omnisint
 otx
-projecdiscovery
+pentesttools
+projectdiscovery
+qwant
 rapiddns
+rocketreach
+securityTrails
 sublist3r
 threatcrowd
-trello
+threatminer
 urlscan
-vhost
 virustotal
+yahoo
 zoomeye' > /tmp/hitempsource.txt
 
 # Find subdomain:
-while read source ; do theHarvester -d "${OSINTDNSTARGET}" -b $source ;done < /tmp/hitempsource.txt |  grep -Eo "[a-zA-Z0-9./?=_-]*[:]([0-9]{1,3}[.]){3}[0-9]{1,3}" | sort -u | tr : "\t"  
-
+while read source ; do theHarvester -d "${OSINTDNSTARGET}" -b $source ;done < /tmp/hitempsource.txt |  grep -Eo "[a-zA-Z0-9./?=_-]*[:]([0-9]{1,3}[.]){3}[0-9]{1,3}" | sort -u | tr : "\t" | tee $2
 
 # Find subdomain:
 # while read source ; do theHarvester -d "${TARGET}" -b $source ;done < /tmp/hitempsource.txt
